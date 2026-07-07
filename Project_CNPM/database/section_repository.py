9ccf32c100b37e49cced098338cf1a1ba3d526f6
lengocanh270706-maker <lengocanh270706@gmail.com>
2026-07-07 13:27:31 +0700

    Update section_repository.py

--- Project_CNPM/database/section_repository.py
+++ Project_CNPM/database/section_repository.py
@@ -1,21 +1,211 @@
 from database.database import Database
 
+
 class SectionRepository:
+
     def __init__(self):
         self.db = Database()
+        self.conn = self.db.connect()
+
+    # ==========================================
+    # Load all Course Classes
+    # ==========================================
+
     def get_all(self):
-        conn = self.db.connect()
-        cursor = conn.cursor()
+
+        cursor = self.conn.cursor()
+
         cursor.execute("""
             SELECT
+                cc.classID,
+                s.subjectName,
+                l.fullName,
+                cc.dayOfWeek,
+                CONCAT(
+                    CONVERT(VARCHAR(5),cc.startTime,108),
+                    ' - ',
+                    CONVERT(VARCHAR(5),cc.endTime,108)
+                ) AS Schedule,
+                cc.room,
+                cc.maxCapacity,
+                cc.status
+            FROM CourseClass cc
+
+            JOIN Subject s
+                ON cc.subjectID=s.subjectID
+
+            JOIN Lecturer l
+                ON cc.lecturerID=l.lecturerID
+
+            ORDER BY cc.classID
+        """)
+
+        rows = cursor.fetchall()
+        for r in rows:
+            print(r)     
+        return rows
+
+    # ==========================================
+    # Subject Combobox
+    # ==========================================
+
+    def get_subjects(self):
+
+        cursor = self.conn.cursor()
+
+        cursor.execute("""
+            SELECT
+                subjectID,
+                subjectName
+            FROM Subject
+            ORDER BY subjectID
+        """)
+
+        return cursor.fetchall()
+
+    # ==========================================
+    # Lecturer Combobox
+    # ==========================================
+
+    def get_lecturers(self):
+
+        cursor = self.conn.cursor()
+
+        cursor.execute("""
+            SELECT
+                lecturerID,
+                fullName
+            FROM Lecturer
+            WHERE status='Active'
+            ORDER BY lecturerID
+        """)
+
+        return cursor.fetchall()
+
+    # ==========================================
+    # Add Section
+    # ==========================================
+
+    def add(
+            self,
+            class_id,
+            subject_id,
+            lecturer_id,
+            period_id,
+            room,
+            day,
+            start,
+            end,
+            capacity,
+            status):
+
+        cursor = self.conn.cursor()
+
+        cursor.execute("""
+
+            INSERT INTO CourseClass
+            (
                 classID,
                 subjectID,
                 lecturerID,
                 periodID,
-                maxCapacity
-            FROM CourseClass
-            ORDER BY classID
-        """)
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
+                room,
+                dayOfWeek,
+                startTime,
+                endTime,
+                maxCapacity,
+                currentEnrolled,
+                status
+            )
+
+            VALUES
+            (
+                ?,?,?,?,?,?,?,?,?,?,?
+            )
+
+        """,
+
+        (
+            class_id,
+            subject_id,
+            lecturer_id,
+            period_id,
+            room,
+            day,
+            start,
+            end,
+            capacity,
+            0,
+            status
+        ))
+
+        self.conn.commit()
+
+    # ==========================================
+    # Update Section
+    # ==========================================
+
+    def update(
+            self,
+            class_id,
+            subject_id,
+            lecturer_id,
+            room,
+            day,
+            start,
+            end,
+            capacity,
+            status):
+
+        cursor = self.conn.cursor()
+
+        cursor.execute("""
+
+            UPDATE CourseClass
+
+            SET
+
+                subjectID=?,
+                lecturerID=?,
+                room=?,
+                dayOfWeek=?,
+                startTime=?,
+                endTime=?,
+                maxCapacity=?,
+                status=?
+
+            WHERE classID=?
+
+        """,
+
+        (
+            subject_id,
+            lecturer_id,
+            room,
+            day,
+            start,
+            end,
+            capacity,
+            status,
+            class_id
+        ))
+
+        self.conn.commit()
+
+    # ==========================================
+    # Delete
+    # ==========================================
+
+    def delete(self,class_id):
+
+        cursor=self.conn.cursor()
+
+        cursor.execute("""
+
+            DELETE FROM CourseClass
+
+            WHERE classID=?
+
+        """,(class_id,))
+
+        self.conn.commit()
